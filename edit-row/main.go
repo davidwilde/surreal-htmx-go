bff78d5aa671e620478283f95299b7a418a3e2dc
David Wilde <d.wilde@selligence.com>
2023-09-25 21:12:10 +0100

    ChatGPT all the DB for Vercel

--- edit-row/main.go
+++ edit-row/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
-	"text/template"
+
+	_ "github.com/lib/pq"
 )
 
 type Person struct {
@@ -14,40 +17,103 @@ type Person struct {
 	Email string
 }
 
-func main() {
-	people := [4]Person{
-		{Id: 0, Name: "Joe Smith", Email: "joe@smith.org"},
-		{Id: 1, Name: "Angie McDowell", Email: "angie@macdowell.org"},
-		{Id: 2, Name: "Fuqua Tarkenton", Email: "fuqua@tarkenton.org"},
-		{Id: 3, Name: "Kim Yee", Email: "kim@yee.org"},
-	}
+var db *sql.DB
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		parts := strings.Split(r.URL.Path, "/")
-		if (len(parts) >= 3) && (parts[1] == "contact") {
-			contactID, err := strconv.Atoi(parts[2])
-			if (err == nil) && (contactID >= 0) && (contactID < len(people)) {
-				if len(parts) == 4 {
-					action := parts[3]
-					if action == "edit" {
-						t := template.Must(template.ParseFiles("edit.html"))
-						t.ExecuteTemplate(w, "person", people[contactID])
-						return
-					}
-				}
-				if r.Method == "PUT" {
-					people[contactID].Name = r.FormValue("name")
-					people[contactID].Email = r.FormValue("email")
-				}
-				t := template.Must(template.ParseFiles("row.html"))
-				t.ExecuteTemplate(w, "person", people[contactID])
+func init() {
+	// Initialize the database connection.
+	dbURL := os.Getenv("POSTGRES_URL")
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+	db = conn
+}
+
+func main() {
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) >= 3 && parts[1] == "contact" {
+		contactID, err := strconv.Atoi(parts[2])
+		if err != nil {
+			http.Error(w, "Invalid contact ID", http.StatusBadRequest)
+			return
+		}
+
+		// Query the database to retrieve the person by ID.
+		person, err := getPersonByID(contactID)
+		if err != nil {
+			http.Error(w, "Failed to retrieve contact", http.StatusInternalServerError)
+			return
+		}
+
+		if len(parts) == 4 {
+			action := parts[3]
+			if action == "edit" {
+				t := template.Must(template.ParseFiles("edit.html"))
+				t.ExecuteTemplate(w, "person", person)
 				return
 			}
-
 		}
-		t := template.Must(template.ParseFiles("index.html", "row.html"))
-		t.Execute(w, people)
-	})
 
-	log.Fatal(http.ListenAndServe(":5432", nil))
+		if r.Method == "PUT" {
+			// Handle the PUT request to update person data in the database.
+			err := updatePerson(person, r.FormValue("name"), r.FormValue("email"))
+			if err != nil {
+				http.Error(w, "Failed to update contact", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		t := template.Must(template.ParseFiles("row.html"))
+		t.ExecuteTemplate(w, "person", person)
+		return
+	}
+
+	// Handle the index page with a list of people.
+	people, err := getAllPeople()
+	if err != nil {
+		http.Error(w, "Failed to retrieve people", http.StatusInternalServerError)
+		return
+	}
+
+	t := template.Must(template.ParseFiles("index.html", "row.html"))
+	t.Execute(w, people)
+}
+
+func getPersonByID(id int) (Person, error) {
+	var person Person
+	err := db.QueryRow("SELECT id, name, email FROM people WHERE id = $1", id).Scan(&person.Id, &person.Name, &person.Email)
+	if err != nil {
+		return person, err
+	}
+	return person, nil
+}
+
+func updatePerson(person Person, newName, newEmail string) error {
+	_, err := db.Exec("UPDATE people SET name = $1, email = $2 WHERE id = $3", newName, newEmail, person.Id)
+	return err
+}
+
+func getAllPeople() ([]Person, error) {
+	rows, err := db.Query("SELECT id, name, email FROM people")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var people []Person
+	for rows.Next() {
+		var person Person
+		err := rows.Scan(&person.Id, &person.Name, &person.Email)
+		if err != nil {
+			return nil, err
+		}
+		people = append(people, person)
+	}
+
+	return people, nil
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module surreal-htmx-go
+
+go 1.21.1
+
+require github.com/lib/pq v1.10.9 // indirect
