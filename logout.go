b3e88d0d4c975ce7b1849a44bd295a66e0618fc1
David Wilde <d.wilde@selligence.com>
2023-11-05 21:58:27 +0000

    Logout to endpoint
    Logging out and just clearing the session doesn't seem good enough. There is
    obviously a cookie on the auth0 endpoint that causes the user to
    automatically log back in.

--- logout.go
+++ logout.go
@@ -3,9 +3,10 @@ package main
 import "net/http"
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Info("Logout handler out")
+	logger.Info("Logout handler")
 	session, _ := sessionStore.Get(r, "session")
 	session.Options.MaxAge = -1
 	session.Save(r, w)
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+    url := oauthConfig.Endpoint.AuthURL + "/logout"
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
